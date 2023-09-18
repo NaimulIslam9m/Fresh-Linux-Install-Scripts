fc635e28370b8ec798e8b6302a1267ea33ccff47
Naimul Islam <naimul9m@gmail.com>
2023-09-18 11:55:19 +0600

    Update kali-zsh.sh

--- kali-zsh.sh
+++ kali-zsh.sh
@@ -1,6 +1,6 @@
 cd
-wget https://gitlab.com/kalilinux/packages/kali-defaults/-/raw/kali/master/etc/skel/.zshrc
+wget -P $HOME https://gitlab.com/kalilinux/packages/kali-defaults/-/raw/kali/master/etc/skel/.zshrc
 sudo apt install zsh-autosuggestions
 sudo apt install zsh-syntax-highlighting
 sudo apt install command-not-found
-echo "source /etc/zsh_command_not_found" >> ~/.zshrcv
+echo "source /etc/zsh_command_not_found" >> ~/.zshrc
